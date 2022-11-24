10d2d43718609c5667768649066a5d2db36ccaac
ltwheels <lt.wheels2019@gmail.com>
2022-11-23 19:40:34 -0700

    added eframe dependency

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -4,15 +4,5 @@ use gameops::{Game, setup_games};
 #[tokio::main]
 async fn main() 
 {
-  let games: Vec<Game> =  setup_games().await;
-
-  for game in games 
-  {
-    if game.appid.eq("236390")
-    {
-        println!("Running {}", game.name);
-        game.launch()
-    }
-  }
 
 }
