b16b1b0b306a727035cfcb4803f80ccc0fa53f49
ltwheels <lt.wheels2019@gmail.com>
2023-05-18 23:58:19 -0600

    I don't know what I changed because it's been so long since I worked on this.

--- gameops/src/lib.rs
+++ gameops/src/lib.rs
@@ -1,50 +1,48 @@
 use open;
 use rayon::prelude::*;
-use serde_json::{Value};
+use serde_json::Value;
 use std::{
+    ffi::OsStr,
     fs::{self, DirEntry, ReadDir},
-    process,
+    process, thread, time,
 };
 
 const STEAM_PATH_WINDOWS: &str = "C:\\Program Files (x86)\\Steam\\steamapps";
-const STEAM_RUNGAME_STRING: &str =  "steam://rungameid/";
+const STEAM_RUNGAME_STRING: &str = "steam://rungameid/";
 const STEAM_API_PATH: &str = "http://store.steampowered.com/api/appdetails/";
 
 const EPIC_PATH_WINDOWS: &str = "C:\\Program Files\\Epic Games";
 const EPIC_GAMES_LAUNCH_PREFIX: &str = "com.epicgames.launcher://apps/";
 const EPIC_GAMES_LAUNCH_SUFFIX: &str = "?action=launch&silent=true";
 
-pub struct Game
-{
+pub struct Game {
     pub name: String,
-    pub appid: String, 
+    pub appid: String,
     pub store: String,
 }
 
 impl Game {
-    pub fn  launch(&self)
-    {
-        if self.store == "Steam"
-        {
-            let run_string: String = format!("{}{}",STEAM_RUNGAME_STRING, self.appid);
+    pub fn launch(&self) {
+        if self.store == "Steam" {
+            let run_string: String = format!("{}{}", STEAM_RUNGAME_STRING, self.appid);
+            open::that(run_string).unwrap();
+        } else if self.store == "EGS" {
+            let run_string: String = format!(
+                "{}{}{}",
+                EPIC_GAMES_LAUNCH_PREFIX, self.appid, EPIC_GAMES_LAUNCH_SUFFIX
+            );
             open::that(run_string).unwrap();
         }
-        else if self.store == "EGS"
-        {
-            let run_string: String = format!("{}{}{}", EPIC_GAMES_LAUNCH_PREFIX, self.appid, EPIC_GAMES_LAUNCH_SUFFIX);
-            open::that(run_string).unwrap();
-        }
+        thread::sleep(time::Duration::from_millis(250));
         process::exit(0);
     }
 }
 
-fn get_egsgames() -> Vec<Game>
-{
+fn get_egsgames() -> Vec<Game> {
     let mut games: Vec<Game> = Vec::new();
     let folders: ReadDir = fs::read_dir(EPIC_PATH_WINDOWS).unwrap();
 
-    for f in folders
-    {
+    for f in folders {
         let dir: DirEntry = f.unwrap();
         let name: String = dir.file_name().into_string().unwrap();
         println!("{}", name);
@@ -52,19 +50,25 @@ fn get_egsgames() -> Vec<Game>
         let mut appid: String = String::from("NaN");
         let next_path: String = dir.path().as_os_str().to_owned().into_string().unwrap();
 
-        let files = fs::read_dir(next_path).unwrap().next().unwrap().unwrap().path().as_os_str().to_owned().into_string().unwrap();
-        let id_path = fs::read_dir(files).unwrap();
-        for file in id_path
-        {
+        let files = fs::read_dir(next_path)
+            .unwrap()
+            .next()
+            .unwrap()
+            .unwrap()
+            .path()
+            .as_os_str()
+            .to_owned()
+            .into_string()
+            .unwrap();
+        let id_path: ReadDir = fs::read_dir(files).unwrap();
+
+        for file in id_path {
             let entry: DirEntry = file.unwrap();
             let isdir: bool = entry.file_type().unwrap().is_dir();
-            if !isdir
-            {
-                
-                let ext: String = entry.path().extension().unwrap().to_owned().into_string().unwrap();
-                if ext == "mancpn"
-                {
-                    let file_to_read: String = entry.path().as_os_str().to_owned().into_string().unwrap();
+            if !isdir {
+                if entry.path().extension().as_ref().unwrap() == &"mancpn" {
+                    let file_to_read: String =
+                        entry.path().as_os_str().to_owned().into_string().unwrap();
                     let file_contents: String = fs::read_to_string(file_to_read).unwrap();
                     let jsn: Value = serde_json::from_str(&file_contents).unwrap();
                     let mut namespace = jsn["CatalogNamespace"].to_string();
@@ -77,50 +81,49 @@ fn get_egsgames() -> Vec<Game>
                     app_name.pop();
                     app_name.remove(0);
                     appid = format!("{}%3A{}%3A{}", namespace, itemid, app_name);
-
                 }
             }
         }
 
-         let game = Game {
-             name: name,
-             store: store,
-             appid: appid,
-         };
+        let game = Game {
+            name: name,
+            store: store,
+            appid: appid,
+        };
 
-         games.push(game);
+        games.push(game);
     }
     games
 }
 
-fn get_steamids() -> Vec<String>
-{   
+fn get_steamids() -> Vec<String> {
     let mut id_list: Vec<String> = Vec::new();
     let files: ReadDir = fs::read_dir(STEAM_PATH_WINDOWS).unwrap();
-    for file in  files 
-    {
+    for file in files {
         let file_entry: DirEntry = file.unwrap();
         let name: String = file_entry.file_name().into_string().unwrap();
         let id: String = name.chars().filter(|c| c.is_digit(10)).collect();
-        if id.len() > 0 && id != "228980"
-        {
+        if id.len() > 0 && id != "228980" {
             id_list.push(id);
         }
     }
     id_list
 }
 
-fn get_steamgame(id: String) -> Game
-{
+fn get_steamgame(id: String) -> Game {
     let client = reqwest::blocking::Client::new();
 
-    let res: String = client.get(STEAM_API_PATH.to_owned())
-                            .query(&[("appids", &id)])
-                            .send().unwrap().text().unwrap();
+    let res: String = client
+        .get(STEAM_API_PATH.to_owned())
+        .query(&[("appids", &id)])
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
     let jsn: Value = serde_json::from_str(&res).unwrap();
-    
+
     let mut name: String = jsn[id.to_owned()]["data"]["name"].to_string();
-    name = name[1..name.len()-1].to_owned();
+    name = name[1..name.len() - 1].to_owned();
 
     let game = Game {
         name: name,
@@ -130,8 +133,7 @@ fn get_steamgame(id: String) -> Game
     game
 }
 
-pub fn setup_games() -> Vec<Game>
-{
+pub fn setup_games() -> Vec<Game> {
     let mut games: Vec<Game> = Vec::new();
     let steamids: Vec<String> = get_steamids();
 
--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -1,57 +1,45 @@
 use eframe::egui::{self, Align, Layout, ScrollArea, Separator};
-use gameops::{Game, setup_games};
+use gameops::{setup_games, Game};
 
 const PADDING: f32 = 5.0;
 
-fn main() 
-{
-  let native_options = eframe::NativeOptions::default();
-  eframe::run_native("Watermelon Game Launcher", native_options, Box::new(|cc| Box::new(GameLauncher::new(cc))));
+fn main() {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Watermelon Game Launcher",
+        native_options,
+        Box::new(|cc| Box::new(GameLauncher::new(cc))),
+    );
 }
 
-struct GameLauncher{
-  games: Vec<Game>
+struct GameLauncher {
+    games: Vec<Game>,
 }
 
-impl GameLauncher
-{
-  fn new(_cc: &eframe::CreationContext<'_>) -> GameLauncher
-  {
-    GameLauncher
-    {
-      games: setup_games()
+impl GameLauncher {
+    fn new(_cc: &eframe::CreationContext<'_>) -> GameLauncher {
+        GameLauncher {
+            games: setup_games(),
+        }
     }
-  }
 }
 
-impl eframe::App for GameLauncher
-{
-
-  fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame)
-  {
-    egui::CentralPanel::default().show(ctx, |ui| 
-      {
-
-        ScrollArea::vertical().show(ui, |ui|{
-          for g in &self.games 
-          {
-            
-            ui.add_space(PADDING);
-            ui.label(&g.store);
-            ui.label(&g.name);
-            ui.with_layout(Layout::right_to_left(Align::TOP), |ui|
-            {
-              if ui.button("Launch Game").clicked()
-              {
-                g.launch();
-              }
+impl eframe::App for GameLauncher {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ScrollArea::vertical().show(ui, |ui| {
+                for g in &self.games {
+                    ui.add_space(PADDING);
+                    ui.label(&g.name);
+                    ui.with_layout(Layout::right_to_left(Align::TOP), |ui| {
+                        if ui.button("Launch Game").clicked() {
+                            g.launch();
+                        }
+                    });
+                    ui.add_space(PADDING);
+                    ui.add(Separator::default());
+                }
             });
-            ui.add_space(PADDING);
-            ui.add(Separator::default());
-  
-          }
         });
-
-      });   
-  }
+    }
 }
